d942fbdc74bd0dd44f74e9ca5046444471d0237c
Ruslan Sanigullin <hirudorar07@gmail.com>
2025-06-14 19:26:26 +0300

    fix: add render domain to ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["webserver"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "webserver").split(",")
 
 
 # Application definition
