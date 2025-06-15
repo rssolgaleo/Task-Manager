cda4225548c73e003a42b3aa76f8a14280a38bb3
Ruslan Sanigullin <hirudorar07@gmail.com>
2025-06-15 23:05:10 +0300

    refactor: migrate to modular app architecture 2

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,35 +1,9 @@
-"""
-URL configuration for task_manager project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 from django.views.generic import TemplateView
-from task_manager.task_manager_app import views
-from django.contrib.auth.views import LoginView, LogoutView
-from task_manager.task_manager_app.views import UserListView, UserUpdateView, UserDeleteView
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='base.html'), name='base'),
-    path('users/create/', views.UserCreateView.as_view(), name='user_create'),
-    path('login/', LoginView.as_view(template_name="users/login.html"), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path("users/", UserListView.as_view(), name="user_list"),
-    path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
-    path("users/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
+    path('admin/', admin.site.urls),
+    path('users/', include('task_manager.task_manager_app.users.urls')),
 ]
