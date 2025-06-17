8750ca96ece9b5ecee154fc676e5c842a131037a
Ruslan Sanigullin <hirudorar07@gmail.com>
2025-06-18 00:09:09 +0300

    tests: try fix

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from task_manager.task_manager_app.users.tests import *
-from task_manager.task_manager_app.statuses.tests import *
-from task_manager.task_manager_app.tasks.tests import *
-from task_manager.task_manager_app.labels.tests import *
