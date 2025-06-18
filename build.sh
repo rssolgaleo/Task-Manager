a8db41e1b94dbe5824613cc119992f2a0706f993
Ruslan Sanigullin <hirudorar07@gmail.com>
2025-06-18 03:52:45 +0300

    feat: edit build.sj

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+source "$HOME/.local/bin/env"
+
+ln -sf "$PWD" code
 
 make install && make collectstatic && make migrate
