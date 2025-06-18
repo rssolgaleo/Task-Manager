7cf0b85f8684904d36f74e4522056f780faf6c8f
Ruslan Sanigullin <hirudorar07@gmail.com>
2025-06-18 03:13:23 +0300

    fix: lol

--- manage.py
+++ manage.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 def main():
-    """Run administrative tasks."""
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, current_path)
-    sys.path.insert(0, os.path.join(current_path, "task_manager"))
-
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        raise ImportError("Couldn't import Django") from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
